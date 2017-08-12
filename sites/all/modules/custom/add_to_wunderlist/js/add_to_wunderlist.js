d6759805abaa4cfb3d2ec7d7334a85f96310cbb6
Jonas Kortum <kortumj@uni-hildesheim.de>
2017-08-12 21:53:38 +0200

    bugfix: add to wunderlist

--- sites/all/modules/custom/add_to_wunderlist/js/add_to_wunderlist.js
+++ sites/all/modules/custom/add_to_wunderlist/js/add_to_wunderlist.js
@@ -2,12 +2,12 @@ function searchsomething(zahl) {
 
     var zahl = document.getElementsByName('quantity')[0].value;
 
+
     if(zahl <50) {
         var counter = 0;
         var test = 'edit-ios-'+counter;
 
         while (document.getElementById(test)) {
-            //alert(counter);
 
             var label = document.querySelector("#"+test+"+ label").innerText;
 
@@ -16,13 +16,16 @@ function searchsomething(zahl) {
             if(space >0) {
 
                 var rechnung = label.substr(0,space);
-                //rechnung = rechnung / zahl * (zahl+1);
+
+                if (!isNaN(rechnung.charAt(0))) {
+                    rechnung = (rechnung/zahl*(parseInt(zahl)+1));
 
 
-                rechnung = (rechnung/zahl*(parseInt(zahl)+1));
+                    document.querySelector("#"+test+"+ label").innerText = rechnung +" "+ label.substr(space+1);
+                }
 
 
-                document.querySelector("#"+test+"+ label").innerText = rechnung +" "+ label.substr(space+1);
+
 
 
 
@@ -61,18 +64,13 @@ function searchsomething2(zahl) {
             if(space >0) {
 
                 var rechnung = label.substr(0,space);
-                //rechnung = rechnung / zahl * (zahl+1);
+
+                if (!isNaN(rechnung.charAt(0))) {
+                    rechnung = (rechnung/zahl*(parseInt(zahl)-1));
 
 
-                rechnung = (rechnung/zahl*(parseInt(zahl)-1));
-
-
-                document.querySelector("#"+test+"+ label").innerText = rechnung +" "+ label.substr(space+1);
-
-
-
-
-
+                    document.querySelector("#"+test+"+ label").innerText = rechnung +" "+ label.substr(space+1);
+                }
             }
 
             counter++;
